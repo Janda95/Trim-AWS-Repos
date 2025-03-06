20c95b0a43724fce58b21b8d3bc0f92828badf7c
Jarek Rettinghouse <j.rettinghouse@gmail.com>
2025-03-05 20:05:43 -0800

    added pagination function, added additional debug logging, standardized sytax

--- trim_repos.py
+++ trim_repos.py
@@ -2,95 +2,111 @@
 # NOTE: if debug is flagged, add each skipped interation to tracking ///////// Normal output will specify name, min survivors
 import argparse
 import boto3
+import json
 import logging
 import sys
 import yaml
 
 
-HELP = '''
+
+HELP = """
     This is the help blurb!
-'''
+"""
 
 
-def get_args(operation='no_help_needed'):
+def getArgs(operation="no_help_needed"):
   parser = argparse.ArgumentParser(
     description=HELP
   )
   parser.add_argument(
-    '--filename',
+    "--filename",
     required=True,
     help='Provide yaml file name for ECRs you would like to trim.'
   )
   parser.add_argument(
-    '--delete',
+    "--delete",
     default=False,
-    help='Provide permission'
+    help="Provide permission."
   )
   parser.add_argument(
-    '--debug',
+    "--debug",
     default=False,
     help="Debug will show all interactions: skipped, recommended for deletion, deleted "
   )
-  if operation == 'help':
+  parser.add_argument(
+    "--regid",
+    default=None,
+    help="Debug will show all interactions: skipped, recommended for deletion, "
+  )
+  if operation == "help":
     parser.print_help()
     sys.exit()
 
   return parser.parse_args()
 
 
-def trim(file_name: str, ecr_name: str ):
-  # load contents
+def paginateRes(ecrClient, regId, repoNames) -> dict:
+  """ Create single json item of paginated response from aws."""
+  items = dict()
+  nextPage = True
+  while nextPage:
+    # TODO: update with proper values
+    res = ecrClient.describeRepositories(
+      registryId=regId,
+      repositoryNames= repoNames,
+      nextToken="TrimReposToMinWithTtyl",
+    )
+
+    res = json.loads(res)
+
+    # load into dict items
+
+    # if not nextToken:
+    #   nextPage = False
+
+    # res none
+    if res is None:
+      print("No repos available with requested names.")
+      sys.exit(0)
+
+
+def trim(fileName: str, debug: bool, canDelete: bool, regId: str):
+  """Load, sort, check, and remove extra repo instances."""
   try:
-    file = open(file_name, mode="rb")
+    file = open(fileName, mode="rb")
   except OSError:
-    print( f"Could not open/read file: {file_name}")
+    print( f"Could not open/read file: {fileName}")
     sys.exit()
 
   with file:
     contents = yaml.safe_load(file)
   
-  ecr_client = boto3.client(ecr_name)
+  ecrClient = boto3.client("ecr")
 
-  repos_to_request = list()
+  reposToRequest = list()
 
   # check for expected structure
   for group in contents.get("ECRs"):
     name = group.get("name", None)
-    min_age_hours = group.get("min_age_hours", None)
-    min_survivors = group.get("min_survivors", None)
+    minAgeHours = group.get("minAgeHours", None)
+    minSurvivors = group.get("minSurvivors", None)
 
-    # if parser.debug == True:
-        #   msg = f"name: {name}, min_age_hours: {min_age_hours}, and survivors: {min_survivors}"
-        #   print(msg)
+    if debug is True:
+          msg = f"Name: {name}, MinAgeHours: {minAgeHours}, and Survivors: {minSurvivors}"
+          logger.info(msg)
 
-    if name != None:
-      repos_to_request.append( (name, min_age_hours, min_survivors) )    
-    
+    if name is not None:
+      reposToRequest.append( (name, minAgeHours, minSurvivors) )
 
-  # TODO: update with proper values
-  res = ecr_client.describeRepositories(
-    registryId='string',
-    repositoryNames=[
-        'string',
-    ],
-    nextToken='string',
-    maxResults=123
-  ).get('repositories', None)
+  allRepos = paginateRes( ecrClient, map(lambda x: x[0], reposToRequest))
 
-  if res == None:
-    print(f"No repos available with requested names.")
-    sys.exit(0)
-    
-    # for group_of_repos in res.repos
+  for name, minAgeHours, minSurvivors in reposToRequest:
+    activeRepos = allRepos.get(name, None)
 
-  for name, min_age_hours, min_survivors in repos_to_request:
-    active_repos = res.get(name, None)
-
-
-    if len(active_repos) <= min_survivors:
-      if parser.debug == True: 
-        print(active_repos)
-      
+    if len(activeRepos) <= minSurvivors:
+      if debug is True:
+        msg = "Repos: " + str(activeRepos) + " don't need to be trimmed."
+        logging.info(msg)
       continue
 
     # iterate through items, create list with important info, sort by datetime (oldest to newest)
@@ -99,11 +115,12 @@ def trim(file_name: str, ecr_name: str ):
 
 
 def main():
-  args = get_args()
-  trim(args.filename, args.debug, args.delete, args.erc_name)
+  args = getArgs()
+  trim(args.filename, args.debug, args.delete, args.regid)
 
 
 if __name__ == "__main__":
-  logging.info("Started")
+  logger = logging.getLogger(__name__)
+  logger.info("Started")
   main()
-  logging.info("Finished")
+  logger.info("Finished")
